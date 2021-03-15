750ccb72a26e0f8c697b1b3ec96614dc8934d7df
SuzAlexandre <alexandre.rousselet@me.com>
2021-03-15 16:35:02 +0800

    started to implement production interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,10 @@
 package com.company;
 
 import javax.swing.*;
-import java.sql.DriverManager;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 
 public class Main {
@@ -30,33 +26,6 @@ public class Main {
 //            System.out.println("Buzz");
 //        if ((num%5!=0)&&(num%3!=0))
 //            System.out.println(num);
-        Connection conn = null;
-
-        try {
-
-            String dbURL = "jdbc:sqlserver://10.41.32.2";
-            String user = "SZ_user";
-            String pass = "SZuser";
-            conn = DriverManager.getConnection(dbURL, user, pass);
-            if (conn != null) {
-                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
-                System.out.println("Driver name: " + dm.getDriverName());
-                System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: " + dm.getDatabaseProductName());
-                System.out.println("Product version: " + dm.getDatabaseProductVersion());
-            }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                if (conn != null && !conn.isClosed()) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
 
 //        final float PI = 3.14F;
 //        int[][]numbers = {{1,2,3,4},{5,6,7}};
@@ -68,11 +37,70 @@ public class Main {
 //        System.out.println(Math.floor(PI));
 //        System.out.println(result);
 //
-//        JFrame frame = new JFrame("My frame");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(300,300);
-//        JButton myButt = new JButton("Press me");
+
+        // Setup main frame
+        JFrame frame = new JFrame("Production interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+        frame.setLocation(50,50);
+
+        // Adding menu bar
+        JMenuBar menuBar = new JMenuBar();
+
+        // Declare menus
+        JMenu menuMain = new JMenu("Main");
+        JMenu menuOption = new JMenu("Options");
+        JMenu menuHelp = new JMenu("Help");
+
+        // Declare menu items
+        JMenuItem menuItemExit = new JMenuItem("Exit");
+        JMenuItem menuItemDBConnect = new JMenuItem("Database connection");
+        JMenuItem menuItemLanguage = new JMenuItem("Language");
+        JMenuItem menuItemAbout = new JMenuItem("About");
+
+        // Adding menu items
+        menuMain.add(menuItemExit);
+        menuOption.add(menuItemDBConnect);
+        menuOption.add(menuItemLanguage);
+        menuHelp.add(menuItemAbout);
+
+        // Adding menus
+        menuBar.add(menuMain);
+        menuBar.add(menuOption);
+        menuBar.add(menuHelp);
+
+        //Adding menu bar in frame
+        frame.setJMenuBar(menuBar);
+
+        // Declare tabbed pane
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // Setting layout policy
+        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+        JPanel tabPane01 = new JPanel();
+        JPanel tabPane02 = new JPanel();
+        JPanel tabPane03 = new JPanel();
+        JPanel tabPane04 = new JPanel();
+        JPanel tabPane05 = new JPanel();
+
+        tabbedPane.add("Quality check",tabPane01);
+        tabbedPane.add("Packing station",tabPane02);
+        tabbedPane.add("Add scrap",tabPane03);
+        tabbedPane.add("Downtime event",tabPane04);
+        tabbedPane.add("Cutting tools change",tabPane05);
+
+        // Setting up items in quality checks tab
+        JLabel barcodeLabel = new JLabel("Barcode");
+        tabPane01.add(barcodeLabel);
+        JTextField barcodeInput = new JTextField(10);
+        tabPane01.add(barcodeInput);
+        JButton myButt = new JButton("Press me");
+        tabPane01.add(myButt);
+
+        frame.add(tabbedPane, BorderLayout.CENTER);
+
 //        frame.getContentPane().add(myButt);
-//        frame.setVisible(true);
+        frame.setVisible(true);
+
     }
 }
